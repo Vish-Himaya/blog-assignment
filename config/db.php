3be1f7dd3b50cf5fea74cea189a6d30f748d1f2a
Vishmi Himaya <vishmihimaya@gmail.com>
2026-08-04 16:08:22 +0530

    Update db config for live hosting

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'blogapp';
-$username = 'root';
-$password = '';
+$host = 'sql306.infinityfree.com';
+$dbname = 'if0_42573605_blogapp';
+$username = 'if0_42573605';
+$password = '1dzsP0lki2Hv';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
